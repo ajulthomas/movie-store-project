9affb08b6a16369ef4d30a709e322fa5ffcc0bd4
ajulthomas <ajul.thomas@outlook.com>
2021-09-12 11:42:14 +0530

    updates folder structure

--- movie-store-api/src/modules/movie/movie.controller.js
+++ movie-store-api/src/modules/movie/movie.controller.js
@@ -7,7 +7,13 @@ const getMovies = async (req, res) => {
 
 const getMovieById = (req, res) => {};
 
-const addMovies = (req, res) => {};
+const addMovies = (req, res) => {
+  const movies = req.body.movies;
+  const userId = req.body.user_id;
+  if (movies.length > 0 && userId) {
+  } else {
+  }
+};
 
 module.exports = {
   getMovies,
